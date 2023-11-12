9a0f17d7020ed87b10cb236c5eb48c63dddb6349
Cristian Romero <cristiandrj@algartech.com>
2023-11-12 00:55:48 -0500

    cartas voladoras

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -19,6 +19,17 @@ export class InicioComponent implements OnInit {
   @ViewChild('videoElement', { static: false }) videoElement?: ElementRef;
   @ViewChild('playButton', { static: false }) playButton?: ElementRef;
 
+
+  botonEmpresasBlanco: boolean = true;
+  botonEmpresasRojo: boolean = false;
+  botonEmprendimientosBlanco: boolean = true;
+  botonEmprendimientosRojo: boolean = false;
+  infoEmprendimientos: boolean = false;
+  infoEmpresas: boolean = false;
+  espacioRojo: boolean = true;
+
+  marginTopDiv: string = '30px';
+
   constructor(private sanitizer: DomSanitizer, private carruselSVC: CarruselService, private router: Router, private formBuilder: FormBuilder, private showmodal: ModalService){
  
      
@@ -26,8 +37,77 @@ export class InicioComponent implements OnInit {
   }
 
  
+  mostrarInfoEmpresas(){
 
+    this.botonEmpresasBlanco = false;
+    this.botonEmpresasRojo = true;
+    this.infoEmpresas = true;
 
+    if(this.botonEmprendimientosRojo){
+      this.espacioRojo = false;
+    } else {
+      this.espacioRojo = true;
+    }
+
+    if(this.botonEmprendimientosRojo){
+      this.marginTopDiv = '226.5px';
+    } else {
+      this.marginTopDiv = '30px';
+    }
+
+  }
+
+  ocultarInfoEmpresas(){
+
+    this.botonEmpresasBlanco = true;
+    this.botonEmpresasRojo = false;
+    this.infoEmpresas = false;
+
+    if(this.botonEmprendimientosRojo){
+      this.espacioRojo = true;
+    } else {
+      this.espacioRojo = false;
+    }
+
+    if(this.botonEmprendimientosRojo){
+      this.marginTopDiv = '30px';
+    } else {
+      this.marginTopDiv = '226.5px';
+    }
+  }
+
+  mostrarInfoEmprendimientos(){
+
+    this.botonEmprendimientosBlanco = false;
+    this.botonEmprendimientosRojo = true;
+    this.infoEmprendimientos = true;
+
+    if(this.botonEmpresasRojo == true){
+      this.espacioRojo = false;
+    } else  {
+      this.espacioRojo = true;
+    }
+
+    if(this.botonEmpresasRojo){
+      this.marginTopDiv = '226.5px';
+    } else {
+      this.marginTopDiv = '30px';
+    }
+
+  }
+
+  ocultarInfoEmprendimientos(){
+
+    this.botonEmprendimientosBlanco = true;
+    this.botonEmprendimientosRojo = false;
+    this.infoEmprendimientos = false;
+
+    if(this.botonEmpresasRojo == false){
+      this.espacioRojo = true;
+    } else {
+      this.espacioRojo = false;
+    }
+  }
 
   ngOnInit(): void {    
 
