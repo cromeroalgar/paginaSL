b6dc2d046bb0bec2a18e206e44d3f424e3fbc255
Cristian Romero <cristiandrj@algartech.com>
2023-11-16 21:53:36 -0500

    ajuste

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -112,6 +112,8 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {    
 
+    window.scroll(0,0);
+
     const animationContainer = document.getElementById('cerebro');
   
     if (animationContainer) {
@@ -136,77 +138,136 @@ export class InicioComponent implements OnInit {
       });
     }
 
-    const animationContainer4 = document.getElementById('lineach');
+    
+
+    const animationContainer22 = document.getElementById('titulobanner');
+  
+    if (animationContainer22) {
+      const animation22 = lottie.loadAnimation({ container: animationContainer22, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/titulobanner.json' });
+      animation22.addEventListener('DOMLoaded', () => {
+      });
+
+      
+    animation22.setSpeed(0.2);
+    }
+ 
+
+  }
+
+  banderaLinea1 = true;
+
+  
+  banderaLinea2 = true;
+
+  banderaLinea3 = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+        // Obtener la posición actual del scroll en el eje Y
+        const scrollYPos = window.scrollY || window.pageYOffset;
+        console.log('Posición del eje Y:', scrollYPos);
+
+        if(scrollYPos > 100 && this.banderaLinea1 == true){
+
+          this.banderaLinea1 = false;
+
+          const animationContainer5 = document.getElementById('lineaTexto');
+
+          if (animationContainer5) {
+            const animation5 = lottie.loadAnimation({ container: animationContainer5, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineaTexto.json' });
+            animation5.addEventListener('DOMLoaded', () => {
+            });
+      
+            animation5.setSpeed(0.2);
+          }
+
+          const animationContainer6 = document.getElementById('lineaTexto2');
+
+          if (animationContainer6) {
+            const animation6 = lottie.loadAnimation({ container: animationContainer6, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineaTexto.json' });
+            animation6.addEventListener('DOMLoaded', () => {
+            });
+      
+            animation6.setSpeed(0.2);
+      
+          }
+      
+
+        }
+
+        
+        if(scrollYPos > 2000 && this.banderaLinea2 == true){
+
+          this.banderaLinea2 = false;
+
+          const animationContainer7 = document.getElementById('lineaTexto3');
+
+          if (animationContainer7) {
+            const animation7 = lottie.loadAnimation({ container: animationContainer7, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineaTexto.json' });
+            animation7.addEventListener('DOMLoaded', () => {
+            });
+      
+            animation7.setSpeed(0.2);
+      
+          }
+
+          const animationContainer9 = document.getElementById('lineaTexto5');
+
+          if (animationContainer9) {
+            const animation9 = lottie.loadAnimation({ container: animationContainer9, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineaTexto.json' });
+            animation9.addEventListener('DOMLoaded', () => {
+            });
+      
+            animation9.setSpeed(0.2);
+      
+          }
+
+          const animationContainer8 = document.getElementById('lineaTexto4');
+
+          if (animationContainer8) {
+            const animation8 = lottie.loadAnimation({ container: animationContainer8, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineaTexto.json' });
+            animation8.addEventListener('DOMLoaded', () => {
+            });
+      
+            animation8.setSpeed(0.2);
+      
+          }
+        }
+
+        if(scrollYPos > 1100 && this.banderaLinea3 == true){
+
+          this.banderaLinea3 = false;
+
+          const animationContainer4 = document.getElementById('lineach');
 
     if (animationContainer4) {
-      const animation4 = lottie.loadAnimation({ container: animationContainer4, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineapeque.json' });
+      const animation4 = lottie.loadAnimation({ container: animationContainer4, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/lineapeque.json' });
       animation4.addEventListener('DOMLoaded', () => {
       });
 
       animation4.setSpeed(0.1);
 
-      animation4.pause(); 
-
     }
 
-    const animationContainer5 = document.getElementById('lineaTexto');
-
-    if (animationContainer5) {
-      const animation5 = lottie.loadAnimation({ container: animationContainer5, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineaTexto.json' });
-      animation5.addEventListener('DOMLoaded', () => {
+    const animationContainer23 = document.getElementById('titulomapa');
+  
+    if (animationContainer23) {
+      const animation23 = lottie.loadAnimation({ container: animationContainer23, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/titulomapa.json' });
+      animation23.addEventListener('DOMLoaded', () => {
       });
 
-      animation5.setSpeed(0.2);
-    }
+      
+    animation23.setSpeed(0.5);
+    
 
-    const animationContainer6 = document.getElementById('lineaTexto2');
-
-    if (animationContainer6) {
-      const animation6 = lottie.loadAnimation({ container: animationContainer6, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineaTexto.json' });
-      animation6.addEventListener('DOMLoaded', () => {
-      });
-
-      animation6.setSpeed(0.2);
 
     }
 
-    const animationContainer7 = document.getElementById('lineaTexto3');
-
-    if (animationContainer7) {
-      const animation7 = lottie.loadAnimation({ container: animationContainer7, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineaTexto.json' });
-      animation7.addEventListener('DOMLoaded', () => {
-      });
-
-      animation7.setSpeed(0.2);
-
-    }
-
-    const animationContainer8 = document.getElementById('lineaTexto4');
-
-    if (animationContainer8) {
-      const animation8 = lottie.loadAnimation({ container: animationContainer8, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineaTexto.json' });
-      animation8.addEventListener('DOMLoaded', () => {
-      });
-
-      animation8.setSpeed(0.2);
-
-    }
-
-    const animationContainer9 = document.getElementById('lineaTexto5');
-
-    if (animationContainer9) {
-      const animation9 = lottie.loadAnimation({ container: animationContainer9, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/lineaTexto.json' });
-      animation9.addEventListener('DOMLoaded', () => {
-      });
-
-      animation9.setSpeed(0.2);
-
-    }
 
     const animationContainer10 = document.getElementById('colombia');
 
     if (animationContainer10) {
-      const animation10 = lottie.loadAnimation({ container: animationContainer10, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/colombia.json' });
+      const animation10 = lottie.loadAnimation({ container: animationContainer10, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/colombia.json' });
       animation10.addEventListener('DOMLoaded', () => {
       });
 
@@ -219,7 +280,7 @@ export class InicioComponent implements OnInit {
     const animationContainer12 = document.getElementById('mexico');
 
     if (animationContainer12) {
-      const animation12 = lottie.loadAnimation({ container: animationContainer12, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/iconorojo.json' });
+      const animation12 = lottie.loadAnimation({ container: animationContainer12, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/iconorojo.json' });
       animation12.addEventListener('DOMLoaded', () => {
       });
 
@@ -230,7 +291,7 @@ export class InicioComponent implements OnInit {
     const animationContainer13 = document.getElementById('salvador');
 
     if (animationContainer13) {
-      const animation13 = lottie.loadAnimation({ container: animationContainer13, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
+      const animation13 = lottie.loadAnimation({ container: animationContainer13, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
       animation13.addEventListener('DOMLoaded', () => {
       });
 
@@ -241,7 +302,7 @@ export class InicioComponent implements OnInit {
     const animationContainer14 = document.getElementById('peru');
 
     if (animationContainer14) {
-      const animation14 = lottie.loadAnimation({ container: animationContainer14, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
+      const animation14 = lottie.loadAnimation({ container: animationContainer14, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
       animation14.addEventListener('DOMLoaded', () => {
       });
 
@@ -252,7 +313,7 @@ export class InicioComponent implements OnInit {
     const animationContainer15 = document.getElementById('argentina');
 
     if (animationContainer15) {
-      const animation15 = lottie.loadAnimation({ container: animationContainer15, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/panamaArgentina.json' });
+      const animation15 = lottie.loadAnimation({ container: animationContainer15, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/panamaArgentina.json' });
       animation15.addEventListener('DOMLoaded', () => {
       });
 
@@ -263,7 +324,7 @@ export class InicioComponent implements OnInit {
     const animationContainer16 = document.getElementById('panama');
 
     if (animationContainer16) {
-      const animation16 = lottie.loadAnimation({ container: animationContainer16, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/panamaArgentina.json' });
+      const animation16 = lottie.loadAnimation({ container: animationContainer16, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/panamaArgentina.json' });
       animation16.addEventListener('DOMLoaded', () => {
       });
 
@@ -275,7 +336,7 @@ export class InicioComponent implements OnInit {
     const animationContainer17 = document.getElementById('guatemala');
 
     if (animationContainer17) {
-      const animation17 = lottie.loadAnimation({ container: animationContainer17, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/DominicanaGuatemala.json' });
+      const animation17 = lottie.loadAnimation({ container: animationContainer17, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/DominicanaGuatemala.json' });
       animation17.addEventListener('DOMLoaded', () => {
       });
 
@@ -288,7 +349,7 @@ export class InicioComponent implements OnInit {
     const animationContainer18 = document.getElementById('dominicana');
 
     if (animationContainer18) {
-      const animation18 = lottie.loadAnimation({ container: animationContainer18, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
+      const animation18 = lottie.loadAnimation({ container: animationContainer18, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/mexicoSalvadorPeru.json' });
       animation18.addEventListener('DOMLoaded', () => {
       });
 
@@ -299,7 +360,7 @@ export class InicioComponent implements OnInit {
     const animationContainer19 = document.getElementById('ecuador');
 
     if (animationContainer19) {
-      const animation19 = lottie.loadAnimation({ container: animationContainer19, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/DominicanaGuatemala.json' });
+      const animation19 = lottie.loadAnimation({ container: animationContainer19, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/DominicanaGuatemala.json' });
       animation19.addEventListener('DOMLoaded', () => {
       });
 
@@ -310,7 +371,7 @@ export class InicioComponent implements OnInit {
     const animationContainer20 = document.getElementById('chile');
 
     if (animationContainer20) {
-      const animation20 = lottie.loadAnimation({ container: animationContainer20, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/iconorojo.json' });
+      const animation20 = lottie.loadAnimation({ container: animationContainer20, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/iconorojo.json' });
       animation20.addEventListener('DOMLoaded', () => {
       });
 
@@ -322,7 +383,7 @@ export class InicioComponent implements OnInit {
     const animationContainer21 = document.getElementById('honduras');
 
     if (animationContainer21) {
-      const animation21 = lottie.loadAnimation({ container: animationContainer21, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/iconorojo.json' });
+      const animation21 = lottie.loadAnimation({ container: animationContainer21, renderer: 'svg', loop: false, autoplay: true, path: 'assets/json/iconorojo.json' });
       animation21.addEventListener('DOMLoaded', () => {
       });
 
@@ -331,39 +392,12 @@ export class InicioComponent implements OnInit {
     }
 
 
-    const animationContainer22 = document.getElementById('titulobanner');
-  
-    if (animationContainer22) {
-      const animation22 = lottie.loadAnimation({ container: animationContainer22, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/titulobanner.json' });
-      animation22.addEventListener('DOMLoaded', () => {
-      });
+        }
 
-      
-    animation22.setSpeed(0.2);
-    }
-
-    const animationContainer23 = document.getElementById('titulomapa');
-  
-    if (animationContainer23) {
-      const animation23 = lottie.loadAnimation({ container: animationContainer23, renderer: 'svg', loop: true, autoplay: true, path: 'assets/json/titulomapa.json' });
-      animation23.addEventListener('DOMLoaded', () => {
-      });
-
-      
-    animation23.setSpeed(0.5);
-    
-
-
-    }
-
-  
-  
-    
-
-  }
 
 
 
+  }
 
 
 
