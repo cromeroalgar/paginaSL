662eae13c3258bce873e50abae5564f6fbcc5b58
cromeroike <cromero@ikeasistencia.com.co>
2023-12-11 11:02:41 -0500

    ajustesd

--- src/app/compartido/header/header.component.ts
+++ src/app/compartido/header/header.component.ts
@@ -19,6 +19,40 @@ export class HeaderComponent{
   ocultar = 0;
   mostarminibanner:boolean = false;
 
+  isUnderlined: boolean = false;
+  
+  isUnderlinedModel: boolean = false;
+
+  isUnderlinedSoluciones: boolean = false;
+
+  isUnderlinedBlog: boolean = false;
+
+  toggleUnderline() {
+    this.isUnderlined = !this.isUnderlined;
+    this.isUnderlinedModel = false;
+  }
+
+  toggleUnderlineModel(){
+    this.isUnderlinedModel = !this.isUnderlinedModel;
+    this.isUnderlined = false;
+    this.isUnderlinedBlog = false;
+    this.isUnderlinedSoluciones = false;
+  }
+
+  toggleUnderlineSoluciones(){
+    this.isUnderlinedSoluciones = !this.isUnderlinedSoluciones;
+    this.isUnderlined = false;
+    this.isUnderlinedModel = false;
+    this.isUnderlinedBlog = false;
+  }
+
+  toggleUnderlineBlog(){
+    this.isUnderlinedBlog = !this.isUnderlinedBlog;
+    this.isUnderlined = false;
+    this.isUnderlinedModel = false;
+    this.isUnderlinedSoluciones = false;
+  }
+
   constructor(@Inject(DOCUMENT) private document:Document, private router: Router, private showmodal: ModalService){
     router.events.subscribe(s => {
 
