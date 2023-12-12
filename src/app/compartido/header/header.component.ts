a2c7ee80e6044a98cef488db5aec301d58cae041
Hectorv26 <hjvallejo20@misena.edu.co>
2023-12-12 12:02:23 -0500

    Ajuste Hover NAVBAR Y Underline FINAL :DD

--- src/app/compartido/header/header.component.ts
+++ src/app/compartido/header/header.component.ts
@@ -1,80 +1,78 @@
 import { ModalService } from './../servicios/modal.service';
-import { Component, HostListener,Inject} from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 
-
 @Component({
   selector: 'app-header',
-  templateUrl: `header.component.html`,
+  templateUrl: 'header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent{
-  scrolled:boolean=true; 
-  lastScroll:number=0;
-  now:number=0;
-  formEnviado$ = this.showmodal.formEnviado$
-  btnclickclose$ = this.showmodal.btnclickclose$
+export class HeaderComponent {
+  scrolled: boolean = true;
+  lastScroll: number = 0;
+  now: number = 0;
+  formEnviado$ = this.showmodal.formEnviado$;
+  btnclickclose$ = this.showmodal.btnclickclose$;
 
   ocultar = 0;
-  mostarminibanner:boolean = false;
+  mostarminibanner: boolean = false;
 
   isUnderlined: boolean = false;
-  
   isUnderlinedModel: boolean = false;
-
   isUnderlinedSoluciones: boolean = false;
-
   isUnderlinedBlog: boolean = false;
 
   toggleUnderline() {
-    this.isUnderlined = !this.isUnderlined;
+    this.isUnderlined = true;
     this.isUnderlinedModel = false;
-  }
-
-  toggleUnderlineModel(){
-    this.isUnderlinedModel = !this.isUnderlinedModel;
-    this.isUnderlined = false;
-    this.isUnderlinedBlog = false;
     this.isUnderlinedSoluciones = false;
-  }
-
-  toggleUnderlineSoluciones(){
-    this.isUnderlinedSoluciones = !this.isUnderlinedSoluciones;
-    this.isUnderlined = false;
-    this.isUnderlinedModel = false;
     this.isUnderlinedBlog = false;
   }
 
-  toggleUnderlineBlog(){
-    this.isUnderlinedBlog = !this.isUnderlinedBlog;
+  toggleUnderlineOff() {
+    this.isUnderlined = false;
+  }
+
+  toggleUnderlineModel() {
+    this.isUnderlinedModel = true;
+    this.isUnderlined = false;
+    this.isUnderlinedSoluciones = false;
+    this.isUnderlinedBlog = false;
+  }
+
+  toggleUnderlineSoluciones() {
+    this.isUnderlinedSoluciones = true;
+    this.isUnderlined = false;
+    this.isUnderlinedModel = false;
+    this.isUnderlinedBlog = false;
+  }
+
+  toggleUnderlineBlog() {
+    this.isUnderlinedBlog = true;
     this.isUnderlined = false;
     this.isUnderlinedModel = false;
     this.isUnderlinedSoluciones = false;
   }
 
-  constructor(@Inject(DOCUMENT) private document:Document, private router: Router, private showmodal: ModalService){
+  keepUnderline() {
+    // No necesitas esta función en este escenario, ya que cada función de toggle maneja el subrayado individualmente.
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private showmodal: ModalService) {
     router.events.subscribe(s => {
-
-
-    })
+      // Aquí puedes agregar lógica adicional si es necesario
+    });
   }
 
-
-  @HostListener("window:scroll",[])  
-  onWindowScroll(){      
-    
-    this.now = window.scrollY  
-    if( this.now >this.lastScroll && this.scrolled ){
-      this.scrolled = false      
-    
-    }else if (this.now <this.lastScroll && !this.scrolled ) {
-      this.scrolled = true        
-    } 
-    this.lastScroll = this.now  
-   
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.now = window.scrollY;
+    if (this.now > this.lastScroll && this.scrolled) {
+      this.scrolled = false;
+    } else if (this.now < this.lastScroll && !this.scrolled) {
+      this.scrolled = true;
+    }
+    this.lastScroll = this.now;
   }
-
-
-
 }
